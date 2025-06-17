bc95dcd041e8717891ab007a3cdd2a29c24089b3
x14119641 <daniel.gil.romero24@gmail.com>
2025-06-17 17:08:23 +0200

    added better printer when i am showing the content in directory

--- file_browser.py
+++ file_browser.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import shutil
 
 AUDIO_EXTENSIONS = {".mp3", ".flac", ".wav", ".ogg", ".aac", ".m4a", ".m3u"}
 def get_home_music_path():
@@ -10,13 +10,25 @@ def list_files(current_dir:Path):
         print("Path does not exists!")
         return
     
-        # Order folders and files
-    entries = sorted(current_dir.iterdir(), key=lambda e: (not e.is_dir(), e.name.lower()))
-    for entry in entries:
-        if entry.is_dir() or entry.suffix.lower() in AUDIO_EXTENSIONS:
-            icon = "U+1F5C0" if entry.is_dir() else "U+1F3B5"
-            print(f"{icon} {entry.name}")
+    # Order folders and files
+    entries = sorted(
+        (e for e in current_dir.iterdir() if e.is_dir() or e.suffix.lower() in AUDIO_EXTENSIONS), 
+        key=lambda e: (not e.is_dir(), e.name.lower()))
+    
+    display_names = [
+        f"{'📁' if e.is_dir() else '🎵'} {e.name}" for e in entries
+    ]
 
+    # Get terminal width
+    terminal_width = shutil.get_terminal_size((80,20)).columns
+    max_len = max(len(name) for name in display_names) if display_names else 0
+    col_width = max_len + 4
+    cols = max(1, terminal_width // col_width)
+    # PRint in rows
+    for i in range(0, len(display_names), cols):
+        row = display_names[i:i+cols]
+        print("".join(name.ljust(col_width) for name in row))
+    
 def change_dir(current_dir:Path, new_path=str) -> Path:
     target = (current_dir / new_path).resolve()
     
